825303837cd9047c4ee2fac762b6742e894df174
Jesse Fulton <jessefulton+github@gmail.com>
2012-03-18 18:52:27 -0700

    crawly

--- scripts/googlelinks.js
+++ scripts/googlelinks.js
@@ -1,6 +1,130 @@
-var casper = require('casper').create();
+var casper = require('casper').create({
+	pageSettings: {
+		loadImages: true
+	},
+    verbose: true,
+    logLevel: 'debug'
+});
+var config = require('../conf/config.js');
+
+/*
+var fantomas = Object.create(casper);
+fantomas.renderJSON = function(what) {
+    return this.echo(JSON.stringify(what, null, '  '));
+};
+*/
+
+var seedPages = [ 
+	{
+		"selector": "#main_content"
+		, "url": "http://www.npr.org/sections/politics/"
+	}/*,
+	{
+		"selector": "#content"
+		, "url": "http://www.foxnews.com/politics/index.html"
+	}
+	*/
+];
 
 
+function getLinksToFollow(theSelector) {
+	var mainDiv = document.querySelector(theSelector);
+	var foundLinks = mainDiv.querySelectorAll("a[href]:not([href^='javascript:']):not([href*=doubleclick]):not([href^='itpc://']):not([href^='zune://']):not([href^='#'])");
+	foundLinks = foundLinks ? foundLinks : [];
+	return Array.prototype.map.call(foundLinks, function(e) {
+		var s = e.getAttribute('href');
+		if (s) {
+			var n = s.indexOf('#');
+			s = s.substring(0, n != -1 ? n : s.length);
+			return s;
+		}
+		else {
+			return e.outerHTML;
+		}
+	});
+	
+}
+
+//http://news.cnet.com/8301-17939_109-10151227-2.html
+casper.start().each(config.users, function(self, user) {
+
+	self.log("Logging out", "debug");
+	self.thenOpen("https://accounts.google.com/Logout");
+	self.log("Logging in user " + user.email, "debug");
+	self.thenOpen("https://accounts.google.com/Login", function() {
+		this.fill('form#gaia_loginform', {
+			'Email': user.email
+			, 'Passwd': user.password
+		}, true);
+	});
+	
+	var loginSuccess =  self.thenEvaluate(function(eml) {
+		return document.querySelector("#gbgs4dn").innerText == eml;
+	}, {"eml": user.email});
+	
+	if (loginSuccess) {
+		self.log("Logged in " + user.email + " successfully", "info");
+	}
+	else {
+		self.log("Could not log in " + user.email, "error");
+	}
+	
+	
+
+	var seedPage = user.seed;
+	self.thenOpen(seedPage.url, function() {
+		var links = [];
+		this.log("Beginning for " + this.getCurrentUrl() + " ("+this.getTitle()+")", "info");
+		links = casper.evaluate(getLinksToFollow, {"theSelector": seedPage.selector});
+
+		for (var i=0; i<links.length; i++) {
+			/*
+			casper.thenOpen(links[i], function() {
+				this.log("\tFollowed link to " + this.getCurrentUrl() + " ("+this.getTitle()+")", "INFO");				
+			});		
+			*/
+			//this.log("\t" + links[i], "debug");
+		}
+		
+	});
+	
+});
+
+
+
+
+
+casper.run(function() {
+	this.echo("done", "INFO").exit();
+});
+
+
+
+/*
+//casper.echo(JSON.stringify(config.users));
+i=0;
+casper.start().each(config.users, function(self, user) {
+	self.echo("going through user" + i);
+	self.thenOpen("https://accounts.google.com/Logout");
+	self.echo("logged out");
+	self.thenOpen("https://accounts.google.com/Login", function() {
+		this.fill('form#gaia_loginform', {
+			'Email': user.email
+			, 'Passwd': user.password
+		}, true);
+	});
+	self.echo("logged in " + user.email + ":" + user.password);	
+	self.thenOpen('https://www.google.com/search?q=' + "foo", function() {
+		this.capture((++i) + 'searchpage.png');
+	});
+	self.echo("searched");
+});
+
+*/
+
+
+//casper.exit();
+/*
 if (!(casper.cli.has("email")) || !(casper.cli.has("password")) || !(casper.cli.has("query"))) {
 	casper.echo("Requires arguments --email=% --password=% --query=%").exit();
 }
@@ -46,7 +170,7 @@ casper.thenOpen('https://www.google.com/search?q=' + query, function() {
 });
 
 
-
 casper.run(function() {
 	this.exit();
 });
+*/
